d7cb4c4f16ab4b37a1f2299c46f390db95bd23cf
CivRoot <ganevvictor@gmail.com>
2022-08-31 19:39:16 +0300

    fixing heroku django missing error

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+web: gunicorn FirstBot.wsgi
 web: pipenv run python src/manage.py runserver 0.0.0.0:$PORT
--- src/core/settings.py
+++ src/core/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
-import django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
